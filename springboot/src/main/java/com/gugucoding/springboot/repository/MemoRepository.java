ab57d242f17137117c73ee2d3d840f10e64f5e7d
kopro-ov <koprodev@gmail.com>
2022-03-08 20:03:54 +0900

    쿼리 메서드와 Pageable의 결합
    - 정렬 조건은 Pageable을 통해서 조절

--- springboot/src/main/java/com/gugucoding/springboot/repository/MemoRepository.java
+++ springboot/src/main/java/com/gugucoding/springboot/repository/MemoRepository.java
@@ -1,6 +1,8 @@
 package com.gugucoding.springboot.repository;
 
 import com.gugucoding.springboot.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +10,7 @@ import java.util.List;
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
     List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
+
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
 
 }
