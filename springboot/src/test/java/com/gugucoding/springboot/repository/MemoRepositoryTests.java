65fa7472af40f2ed8ed22cdd6374e705399ba459
kopro-ov <koprodev@gmail.com>
2022-03-08 19:51:00 +0900

    페이징 처리 테스트 코드 작성

--- springboot/src/test/java/com/gugucoding/springboot/repository/MemoRepositoryTests.java
+++ springboot/src/test/java/com/gugucoding/springboot/repository/MemoRepositoryTests.java
@@ -4,6 +4,9 @@ import com.gugucoding.springboot.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import javax.swing.text.html.Option;
 import javax.transaction.Transactional;
@@ -68,6 +71,16 @@ public class MemoRepositoryTests {
 
         Long mno = 100L;
         memoRepository.deleteById(mno);
+
+    }
+
+    @Test
+    public void testPageDefault() {
+
+        //1페이지 10개
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Memo> result  = memoRepository.findAll(pageable);
+        System.out.println(result);
 
     }
 
