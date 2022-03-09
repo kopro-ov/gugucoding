8d3e350e01c0ff6c9e997363c77f5f39c16c3a21
kopro-ov <koprodev@gmail.com>
2022-03-09 21:38:05 +0900

    Querydsl를 이용하게 되면 QuerydslPredicateExcutor 인터페이스 상속

--- springboot/guestbook/src/main/java/com/gugucoding/guestbook/repository/GuestbookRepository.java
+++ springboot/guestbook/src/main/java/com/gugucoding/guestbook/repository/GuestbookRepository.java
@@ -2,6 +2,7 @@ package com.gugucoding.guestbook.repository;
 
 import com.gugucoding.guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
 }
