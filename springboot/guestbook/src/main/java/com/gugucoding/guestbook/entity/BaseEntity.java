c2cec1a8418456c9a371208424879efd20a4356a
kopro-ov <koprodev@gmail.com>
2022-03-09 21:15:48 +0900

    시간 처리를 위한 BaseEntity 클래스 생성

---
+++ springboot/guestbook/src/main/java/com/gugucoding/guestbook/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.gugucoding.guestbook.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass //해당 어노테이션이 적용된 클래스는 테이블로 생성되지 않음.
+@EntityListeners(value = {AuditingEntityListener.class })
+@Getter
+abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "regDate", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name = "modDate")
+    private LocalDateTime modDate;
+
+}
