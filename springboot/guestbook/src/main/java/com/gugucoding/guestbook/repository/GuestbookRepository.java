c7e8242e2dac6b053e197e0866f6c355f291dae4
kopro-ov <koprodev@gmail.com>
2022-03-09 21:21:32 +0900

    add Guestbook Repository

---
+++ springboot/guestbook/src/main/java/com/gugucoding/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,7 @@
+package com.gugucoding.guestbook.repository;
+
+import com.gugucoding.guestbook.entity.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+}
