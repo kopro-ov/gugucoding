fbc27e7418fe9f1f635d2036c6ae16c849dc0ef5
kopro-ov <koprodev@gmail.com>
2022-03-10 21:08:14 +0900

    페이징 처리 테스트 코드 추가

--- springboot/guestbook/src/test/java/com/gugucoding/guestbook/service/GuestbookServiceTests.java
+++ springboot/guestbook/src/test/java/com/gugucoding/guestbook/service/GuestbookServiceTests.java
@@ -6,7 +6,9 @@ import com.gugucoding.guestbook.dto.PageResultDTO;
 import com.gugucoding.guestbook.entity.Guestbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class GuestbookServiceTests {
 
     @Autowired
@@ -28,13 +30,20 @@ public class GuestbookServiceTests {
     @Test
     public void testList() {
 
-        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+        PageRequestDTO pageResultDTO = PageRequestDTO.builder().page(1).size(10).build();
 
-        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = guestbookService.getList(pageRequestDTO);
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = guestbookService.getList(pageResultDTO);
 
+        System.out.println("PREV : " +resultDTO.isPrev());
+        System.out.println("NEXT : " +resultDTO.isNext());
+        System.out.println("TOTAL : " +resultDTO.getTotalPage());
+
+        System.out.println("---------------------------");
         for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
             System.out.println(guestbookDTO);
         }
+        System.out.println("---------------------------");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
 
 
     }
