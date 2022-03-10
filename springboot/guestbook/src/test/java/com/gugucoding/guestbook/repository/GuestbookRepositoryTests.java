75654b34d9ab7a02bbe66d93dac84b87dd34dbd0
kopro-ov <koprodev@gmail.com>
2022-03-10 20:25:27 +0900

    단일 항목 검색 테스트
    - BooleanBuilder는 where문에 들어가는 조건들을 넣어주는 컨테이너

--- springboot/guestbook/src/test/java/com/gugucoding/guestbook/repository/GuestbookRepositoryTests.java
+++ springboot/guestbook/src/test/java/com/gugucoding/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,9 +1,16 @@
 package com.gugucoding.guestbook.repository;
 
 import com.gugucoding.guestbook.entity.Guestbook;
+import com.gugucoding.guestbook.entity.QGuestbook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.stream.IntStream;
 
@@ -23,6 +30,25 @@ public class GuestbookRepositoryTests {
                     .build();
             System.out.println(guestbookRepository.save(guestbook));
         });
+    }
+
+    @Test
+    public void testQuery1() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        String keyword = "1";
+        BooleanBuilder builder = new BooleanBuilder();
+
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+        builder.and(expression);
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+
     }
 
 }
