6b6fd44d4560a131acae0af59e135592e1c04ca6
kopro-ov <koprodev@gmail.com>
2022-03-10 20:35:18 +0900

    Guestbook 서비스 계층 추가

---
+++ springboot/guestbook/src/main/java/com/gugucoding/guestbook/service/GuestbookService.java
@@ -0,0 +1,9 @@
+package com.gugucoding.guestbook.service;
+
+import com.gugucoding.guestbook.dto.GuestbookDTO;
+
+public interface GuestbookService {
+
+    Long register(GuestbookDTO guestbookDTO);
+
+}
---
+++ springboot/guestbook/src/main/java/com/gugucoding/guestbook/service/impl/GuestbookServiceImpl.java
@@ -0,0 +1,17 @@
+package com.gugucoding.guestbook.service.impl;
+
+import com.gugucoding.guestbook.dto.GuestbookDTO;
+import com.gugucoding.guestbook.service.GuestbookService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class GuestbookServiceImpl implements GuestbookService {
+
+    @Override
+    public Long register(GuestbookDTO guestbookDTO) {
+        return null;
+    }
+
+}
