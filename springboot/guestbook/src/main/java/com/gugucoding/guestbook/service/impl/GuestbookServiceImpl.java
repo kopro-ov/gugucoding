a3143aac354177d27beb6dad68baa20c5f29c128
kopro-ov <koprodev@gmail.com>
2022-03-11 21:44:47 +0900

    검색 조건과 키워드로 검색 처리 기능

--- springboot/guestbook/src/main/java/com/gugucoding/guestbook/service/impl/GuestbookServiceImpl.java
+++ springboot/guestbook/src/main/java/com/gugucoding/guestbook/service/impl/GuestbookServiceImpl.java
@@ -4,8 +4,11 @@ import com.gugucoding.guestbook.dto.GuestbookDTO;
 import com.gugucoding.guestbook.dto.PageRequestDTO;
 import com.gugucoding.guestbook.dto.PageResultDTO;
 import com.gugucoding.guestbook.entity.Guestbook;
+import com.gugucoding.guestbook.entity.QGuestbook;
 import com.gugucoding.guestbook.repository.GuestbookRepository;
 import com.gugucoding.guestbook.service.GuestbookService;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -42,7 +45,10 @@ public class GuestbookServiceImpl implements GuestbookService {
 
         Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
 
-        Page<Guestbook> result = guestbookRepository.findAll(pageable);
+        BooleanBuilder booleanBuilder = getSearch(requestDTO);
+
+        //querydsl 사용
+        Page<Guestbook> result = guestbookRepository.findAll(booleanBuilder, pageable);
 
         Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
 
@@ -78,6 +84,44 @@ public class GuestbookServiceImpl implements GuestbookService {
             guestbookRepository.save(entity);
 
         }
+
+    }
+
+    private BooleanBuilder getSearch(PageRequestDTO requestDTO) {
+
+        String type = requestDTO.getType();
+        String keyword = requestDTO.getKeyword();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        BooleanExpression expression = qGuestbook.gno.gt(0L);
+        booleanBuilder.and(expression);
+
+        if(type == null || type.trim().length() == 0) { //검색 조건이 없는 경우
+            return booleanBuilder;
+        }
+
+        //검색 조건 작성
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+        if (type.contains("t")) {
+            conditionBuilder.or(qGuestbook.title.contains(keyword));
+        }
+        if (type.contains("c")) {
+            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        }
+        if (type.contains("w")) {
+            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        }
+
+        //모든 조건 통합
+        booleanBuilder.and(conditionBuilder);
+
+        return booleanBuilder;
+
+
+
 
     }
 
--- springboot/guestbook/src/test/java/com/gugucoding/guestbook/service/GuestbookServiceTests.java
+++ springboot/guestbook/src/test/java/com/gugucoding/guestbook/service/GuestbookServiceTests.java
@@ -48,4 +48,30 @@ public class GuestbookServiceTests {
 
     }
 
+    @Test
+    public void testSearch() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .type("tc") //검색 조건 t, c, w, tc, tcw..
+                .keyword("ㄴㅇㄹㄴ")
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = guestbookService.getList(pageRequestDTO);
+
+
+        System.out.println("PREV : " +resultDTO.isPrev());
+        System.out.println("NEXT : " +resultDTO.isNext());
+        System.out.println("TOTAL : " +resultDTO.getTotalPage());
+
+        System.out.println("---------------------------");
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+        System.out.println("---------------------------");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+
+    }
+
 }
