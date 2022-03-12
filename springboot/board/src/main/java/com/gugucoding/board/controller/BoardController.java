dabc957136a94428e7bac95c532fd9a167bf47d0
kopro-ov <koprodev@gmail.com>
2022-03-12 22:08:02 +0900

    등록페이지 작업

--- springboot/board/src/main/java/com/gugucoding/board/controller/BoardController.java
+++ springboot/board/src/main/java/com/gugucoding/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.gugucoding.board.controller;
 
+import com.gugucoding.board.dto.BoardDTO;
 import com.gugucoding.board.dto.PageRequestDTO;
 import com.gugucoding.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board/")
@@ -22,6 +25,23 @@ public class BoardController {
 
         log.info("list......"+pageRequestDTO);
         model.addAttribute("result", boardService.getList(pageRequestDTO));
+
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + dto);
+        Long bno = boardService.register(dto);
+
+        log.info("BNO: "+bno);
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
 
     }
 
