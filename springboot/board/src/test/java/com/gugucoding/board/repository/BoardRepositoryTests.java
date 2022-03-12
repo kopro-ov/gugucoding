20266da467cfca1872e22204db13cce4658edcac
kopro-ov <koprodev@gmail.com>
2022-03-12 19:36:54 +0900

    목록 화면에 필요한 쿼리 작성
    - 게시물, 회원정보, 댓글 수

--- springboot/board/src/test/java/com/gugucoding/board/repository/BoardRepositoryTests.java
+++ springboot/board/src/test/java/com/gugucoding/board/repository/BoardRepositoryTests.java
@@ -5,6 +5,10 @@ import com.gugucoding.board.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
@@ -63,6 +67,17 @@ public class BoardRepositoryTests {
         for (Object[] arr : result) {
             System.out.println(Arrays.toString(arr));
         }
+    }
+
+    @Test
+    public void testReadWithReplyCount() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+        result.get().forEach(row -> {
+            Object[] arr = (Object[]) row;
+            System.out.println(Arrays.toString(arr));
+        });
     }
 
 
