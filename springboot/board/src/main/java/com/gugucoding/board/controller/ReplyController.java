2e2f0f41bb71803c25fe5cade765dac549e9d8f4
kopro-ov <koprodev@gmail.com>
2022-03-14 01:18:13 +0900

    댓글 삭제 처리

--- springboot/board/src/main/java/com/gugucoding/board/controller/ReplyController.java
+++ springboot/board/src/main/java/com/gugucoding/board/controller/ReplyController.java
@@ -33,4 +33,13 @@ public class ReplyController {
         return new ResponseEntity<>(rno, HttpStatus.OK);
     }
 
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+
+        log.info(rno);
+
+        replyService.remove(rno);
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
 }
