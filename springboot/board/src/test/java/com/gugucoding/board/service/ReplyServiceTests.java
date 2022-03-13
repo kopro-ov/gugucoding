14c89d4f0a532a5eeaa0d367265319279f99f1a8
kopro-ov <koprodev@gmail.com>
2022-03-14 00:23:37 +0900

    댓글 기능 추가
    - 댓글 등록
    - 특정 게시물의 댓글 리스트 조회
    - 댓글을 수정하고 삭제하는 기능
    - Reply를 ReplyDTO를 변환하는 entityToDTO()
    - ReplyDTO를 Reply로 변환하는 dtoToEntity()

---
+++ springboot/board/src/test/java/com/gugucoding/board/service/ReplyServiceTests.java
@@ -0,0 +1,25 @@
+package com.gugucoding.board.service;
+
+import com.gugucoding.board.dto.ReplyDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class ReplyServiceTests {
+
+    @Autowired
+    private ReplyService replyService;
+
+    @Test
+    public void testGetList() {
+
+        Long bno = 100L;
+        List<ReplyDTO> replyDTOList = replyService.getList(bno);
+        replyDTOList.forEach(replyDTO -> System.out.println(replyDTO));
+
+    }
+
+}
