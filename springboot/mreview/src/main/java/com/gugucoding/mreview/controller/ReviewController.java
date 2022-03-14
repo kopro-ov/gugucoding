56aded97bc1f5c55e36181de7db5d6d07ee16550
kopro-ov <koprodev@gmail.com>
2022-03-14 23:55:03 +0900

    리뷰 crud 컨트롤러 추가

---
+++ springboot/mreview/src/main/java/com/gugucoding/mreview/controller/ReviewController.java
@@ -0,0 +1,53 @@
+package com.gugucoding.mreview.controller;
+
+import com.gugucoding.mreview.dto.ReviewDTO;
+import com.gugucoding.mreview.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reivews")
+@Log4j2
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @GetMapping("/{mno}/all")
+    public ResponseEntity<List<ReviewDTO>> getList(@PathVariable("mno") Long mno) {
+
+        log.info("--------list---------");
+        log.info("MNO : " + mno);
+
+        List<ReviewDTO> reviewDTOList = reviewService.getListOfMovie(mno);
+
+        return new ResponseEntity<>(reviewDTOList, HttpStatus.OK);
+
+    }
+
+    @PutMapping("/{mno}/{reviewnum}")
+    public ResponseEntity<Long> modifyReview(@PathVariable Long reviewnum, @RequestBody ReviewDTO reviewDTO) {
+        log.info("-----modify-------");
+        log.info("reviewDTO : "+reviewDTO);
+
+        reviewService.modify(reviewDTO);
+
+        return new ResponseEntity<>(reviewnum, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{mno}/{reviewnum}")
+    public ResponseEntity<Long> removeReview(@PathVariable Long reviewnum) {
+        log.info("---------removeReview----------");
+        log.info("review : "+reviewnum);
+        reviewService.remove(reviewnum);
+        return new ResponseEntity<>(reviewnum, HttpStatus.OK);
+    }
+
+
+}
