a15a8ae76577420840e0ed45bded7f7618d65a7c
kopro-ov <koprodev@gmail.com>
2022-03-14 21:05:19 +0900

    업로드 이미지 출력 처리

--- springboot/mreview/src/main/java/com/gugucoding/mreview/controller/UploadController.java
+++ springboot/mreview/src/main/java/com/gugucoding/mreview/controller/UploadController.java
@@ -4,14 +4,20 @@ import com.gugucoding.mreview.common.FileUploadUtil;
 import com.gugucoding.mreview.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -64,6 +70,33 @@ public class UploadController {
         }
 
         return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            log.info("fileName: "+srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+            log.info("file: "+file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            //MIME타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            //파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
 
     }
 
