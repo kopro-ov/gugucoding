e98f3e799649522edee44f3f24206ab103e97840
kopro-ov <koprodev@gmail.com>
2022-03-14 20:01:26 +0900

    특정 영화의 모든 리뷰와 회원 정보 조회
    - Review클래스의 Member에 대한 Fetch 방식이 LAZY이기 때문에 한 번에 Review 객체와 Member 조회 할 수 없다.
    
    이 문제를 해결할 수 있는 방법은 크게 보면
    1. @Query를 이용해서 조인
    2. @EntityGraph를 이용해서 Review객체를 가져올 때 Member객체를 로딩.
    
    @EntityGraph
    - 엔티티의 특정한 속성을 같이 로딩하도록 표시하는 어노테이션.

--- springboot/mreview/src/main/java/com/gugucoding/mreview/repository/ReviewRepository.java
+++ springboot/mreview/src/main/java/com/gugucoding/mreview/repository/ReviewRepository.java
@@ -1,7 +1,15 @@
 package com.gugucoding.mreview.repository;
 
+import com.gugucoding.mreview.entity.Movie;
 import com.gugucoding.mreview.entity.Review;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
+    List<Review> findByMovie(Movie movie);
+
 }
--- springboot/mreview/src/test/java/com/gugucoding/mreview/repository/ReviewRepositoryTests.java
+++ springboot/mreview/src/test/java/com/gugucoding/mreview/repository/ReviewRepositoryTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -37,5 +38,21 @@ public class ReviewRepositoryTests {
         });
 
     }
+
+    @Test
+    public void testGetMovieReviews() {
+
+        Movie movie = Movie.builder().mno(92L).build();
+
+        List<Review> result = reviewRepository.findByMovie(movie);
+
+        result.forEach(movieReview -> {
+            System.out.println(movieReview.getReviewnum());
+            System.out.println(movieReview.getMember().getEmail());
+        });
+
+
+    }
+
 
 }
