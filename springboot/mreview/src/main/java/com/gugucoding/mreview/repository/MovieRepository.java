851174c24e90fbc41f14f50d240b580cfecd2312
kopro-ov <koprodev@gmail.com>
2022-03-14 18:50:07 +0900

    특정 영화의 모든 이미지와 평균 편점/리뷰 개수

--- springboot/mreview/src/main/java/com/gugucoding/mreview/repository/MovieRepository.java
+++ springboot/mreview/src/main/java/com/gugucoding/mreview/repository/MovieRepository.java
@@ -6,11 +6,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
             + "left outer join MovieImage mi on mi.movie = m "
             + "left outer join Review r on r.movie = m group by m")
     Page<Object[]> getListPage(Pageable pageable);
+
+    @Query("select m, mi " +
+            " from Movie m left outer join MovieImage mi on mi.movie = m " +
+            " where m.mno = :mno")
+    List<Object[]> getMovieWithAll(Long mno);
 
 }
--- springboot/mreview/src/test/java/com/gugucoding/mreview/repository/MovieRepositoryTests.java
+++ springboot/mreview/src/test/java/com/gugucoding/mreview/repository/MovieRepositoryTests.java
@@ -12,6 +12,7 @@ import org.springframework.test.annotation.Commit;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -49,7 +50,7 @@ public class MovieRepositoryTests {
 
     @Test
     public void testListPage() {
-        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "no"));
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "mno"));
 
         Page<Object[]> result = movieRepository.getListPage(pageRequest);
 
@@ -57,7 +58,17 @@ public class MovieRepositoryTests {
             System.out.println(Arrays.toString(objects));
         }
 
+    }
 
+    @Test
+    public void testGetMovieWithAll() {
+        List<Object[]> result = movieRepository.getMovieWithAll(94L);
+
+        System.out.println(result);
+
+        for (Object[] arr: result) {
+            System.out.println(Arrays.toString(arr));
+        }
     }
 
 }
