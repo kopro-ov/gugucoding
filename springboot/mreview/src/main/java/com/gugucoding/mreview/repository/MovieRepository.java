01c8ec84240b1a241976f0e3f2a2b86798f213c3
kopro-ov <koprodev@gmail.com>
2022-03-14 18:52:36 +0900

    리뷰와 조인한 후 영화 이미지 별로 group by

--- springboot/mreview/src/main/java/com/gugucoding/mreview/repository/MovieRepository.java
+++ springboot/mreview/src/main/java/com/gugucoding/mreview/repository/MovieRepository.java
@@ -15,9 +15,10 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
             + "left outer join Review r on r.movie = m group by m")
     Page<Object[]> getListPage(Pageable pageable);
 
-    @Query("select m, mi " +
+    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(r) " +
             " from Movie m left outer join MovieImage mi on mi.movie = m " +
-            " where m.mno = :mno")
+            " left outer join Review r on r.movie = m"+
+            " where m.mno = :mno group by mi")
     List<Object[]> getMovieWithAll(Long mno);
 
 }
