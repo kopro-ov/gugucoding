a9dc3c68b34962018beecd5a3a3e14301853e2e6
kopro-ov <koprodev@gmail.com>
2022-03-14 20:35:04 +0900

    파일 업로드를 위한  컨트롤러와 화면 테스트

---
+++ springboot/mreview/src/main/java/com/gugucoding/mreview/controller/UploadTestController.java
@@ -0,0 +1,14 @@
+package com.gugucoding.mreview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UploadTestController {
+
+    @GetMapping("/uploadEx")
+    public void uploadEx() {
+
+    }
+
+}
