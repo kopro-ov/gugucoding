c0b70816baf577ac4ed848c7dcaaf03afd3a1d41
kopro-ov <koprodev@gmail.com>
2022-03-14 23:11:45 +0900

    영화 목록 처리

--- springboot/mreview/src/main/java/com/gugucoding/mreview/controller/MovieController.java
+++ springboot/mreview/src/main/java/com/gugucoding/mreview/controller/MovieController.java
@@ -1,10 +1,12 @@
 package com.gugucoding.mreview.controller;
 
 import com.gugucoding.mreview.dto.MovieDTO;
+import com.gugucoding.mreview.dto.PageRequestDTO;
 import com.gugucoding.mreview.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,12 @@ public class MovieController {
 
         return "redirect:/movie/list";
 
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("pageRequestDTO: " + pageRequestDTO);
+        model.addAttribute("result", movieService.getList(pageRequestDTO));
     }
 
 }
--- springboot/mreview/src/main/java/com/gugucoding/mreview/service/impl/MovieServiceImpl.java
+++ springboot/mreview/src/main/java/com/gugucoding/mreview/service/impl/MovieServiceImpl.java
@@ -1,6 +1,8 @@
 package com.gugucoding.mreview.service.impl;
 
 import com.gugucoding.mreview.dto.MovieDTO;
+import com.gugucoding.mreview.dto.PageRequestDTO;
+import com.gugucoding.mreview.dto.PageResultDTO;
 import com.gugucoding.mreview.entity.Movie;
 import com.gugucoding.mreview.entity.MovieImage;
 import com.gugucoding.mreview.repository.MovieImageRepository;
@@ -8,11 +10,16 @@ import com.gugucoding.mreview.repository.MovieRepository;
 import com.gugucoding.mreview.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 @Log4j2
@@ -39,4 +46,23 @@ public class MovieServiceImpl implements MovieService {
 
         return movie.getMno();
     }
+
+    @Override
+    public PageResultDTO<MovieDTO, Object[]> getList(PageRequestDTO requestDTO) {
+
+        Pageable pageable = requestDTO.getPageable(Sort.by("mno").descending());
+
+        Page<Object[]> result = movieRepository.getListPage(pageable);
+
+        Function<Object[], MovieDTO> fn = (arr -> entitiesToDTO(
+                (Movie)arr[0],
+                (List<MovieImage>) (Arrays.asList((MovieImage) arr[1])),
+                (Double) arr[2],
+                (Long) arr[3]
+        ));
+
+        return new PageResultDTO<>(result, fn);
+
+    }
+
 }
