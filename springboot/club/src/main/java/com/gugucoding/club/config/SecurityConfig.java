f28a84e6d841bde39a7b00de9716978d593ebf50
kopro-ov <koprodev@gmail.com>
2022-03-15 21:05:36 +0900

    AuthenticationManager 설정 - 단순 테스트 용도로 user1 / 1111 / USER권한
    Authorization가 필요한 리소스 설정
    - /sample/all url은 별도의 로그인 없이 접근이 가능하게 작업

--- springboot/club/src/main/java/com/gugucoding/club/config/SecurityConfig.java
+++ springboot/club/src/main/java/com/gugucoding/club/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.gugucoding.club.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +17,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //사용자 계정은 user
+        auth.inMemoryAuthentication().withUser("user1")
+                .password("$2a$10$jfEnbgZN152Jd4INiOAPoe14g5uxfsV640jV1onrBdlV1qOlLmy2i")
+                .roles("USER");
     }
 
 }
