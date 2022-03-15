201d729fd8a952d878e2df2f7a2dfe4cdeb8e2f1
kopro-ov <koprodev@gmail.com>
2022-03-15 22:05:19 +0900

    ClubUserDetailsService와 ClubMemberRepository연동
    - ClubMemberRepository를 주입받을 수 있는 구조로 변경하고 @RequiredArgsConstructor처리
    - username이 실제로 ClubMember에서는 email을 의미하므로 이를 사용해서 ClubMemberRepository의 findByEmail()을 호출
    - 사용자가 존재하지 않으면 UsernameNotFoundException처리
    - ClubMember를 UserDetails 타입으로 처리하기 위해 ClubAuthMemberDTO타입으로 변환
    - ClubMemberRole은 스프링 시큐리티에서 사용하는 SimpleGrantedAuthority로 변환 이때 'ROLE_'라는 접두어를 추가해서 사용

--- springboot/club/src/main/java/com/gugucoding/club/security/service/ClubUserDetailsService.java
+++ springboot/club/src/main/java/com/gugucoding/club/security/service/ClubUserDetailsService.java
@@ -1,20 +1,52 @@
 package com.gugucoding.club.security.service;
 
+import com.gugucoding.club.entity.ClubMember;
+import com.gugucoding.club.repository.ClubMemberRepository;
+import com.gugucoding.club.security.dto.ClubAuthMemberDTO;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class ClubUserDetailsService implements UserDetailsService {
+
+    private final ClubMemberRepository clubMemberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         log.info("ClubUserDetailsService loadUserByUsername "+username);
 
-        return null;
+        Optional<ClubMember> result = clubMemberRepository.findByEmail(username, false);
+
+        if(result.isPresent()) {
+            throw new UsernameNotFoundException("Check Email or Social");
+        }
+
+        ClubMember clubMember = result.get();
+
+        log.info("----------------");
+        log.info(clubMember);
+
+        ClubAuthMemberDTO clubAuthMember = new ClubAuthMemberDTO(
+                clubMember.getEmail(),
+                clubMember.getPassword(),
+                clubMember.isFromSocial(),
+                clubMember.getRoleSet().stream()
+                        .map(clubMemberRole -> new SimpleGrantedAuthority("ROLE_"+clubMemberRole.toString())).collect(Collectors.toList())
+        );
+        clubAuthMember.setName(clubAuthMember.getName());
+        clubAuthMember.setFromSocial(clubAuthMember.isFromSocial());
+
+        return clubAuthMember;
     }
 }
