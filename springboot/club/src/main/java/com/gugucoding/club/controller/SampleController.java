33035825df4302146f34c31759b05274ec185f79
kopro-ov <koprodev@gmail.com>
2022-03-15 22:23:49 +0900

    Thymeleaf/Controller에서 사용자 정보 출력하기
    - 스프링 시큐리티 사용 시 Authentication이라는 타입을 이용해서 사용자의 정보 추출.
      (현재 프로젝트에서는 ClubAuthMemberDTO라는 객체)
    - Controller에서 @AuthenticationPrincipal 어노테이션을 사용하여 처리.

--- springboot/club/src/main/java/com/gugucoding/club/controller/SampleController.java
+++ springboot/club/src/main/java/com/gugucoding/club/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.gugucoding.club.controller;
 
+import com.gugucoding.club.security.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,12 @@ public class SampleController {
     }
 
     @GetMapping("/member")
-    public void member() {
+    public void member(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember) {
         log.info("member....");
+
+        log.info("-----------------");
+        log.info(clubAuthMember);
+
     }
 
     @GetMapping("/admin")
