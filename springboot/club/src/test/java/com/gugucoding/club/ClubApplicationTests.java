953613b8c7a3118a5c4b181a77bec0e72dce9c36
kopro-ov <koprodev@gmail.com>
2022-03-15 20:34:32 +0900

    init clud project

---
+++ springboot/club/src/test/java/com/gugucoding/club/ClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gugucoding.club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
