17379ee3b3eea398b9011918604b7979d750687e
kopro-ov <koprodev@gmail.com>
2022-03-17 00:22:04 +0900

    Authorization 헤더 처리

--- springboot/club/src/main/java/com/gugucoding/club/security/filter/ApiCheckFilter.java
+++ springboot/club/src/main/java/com/gugucoding/club/security/filter/ApiCheckFilter.java
@@ -2,6 +2,7 @@ package com.gugucoding.club.security.filter;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -30,12 +31,31 @@ public class ApiCheckFilter extends OncePerRequestFilter {
             log.info("API CHECK FILTER....");
             log.info("API CHECK FILTER....");
 
+            boolean checkHeader = checkAuthHeader(request);
+            if (checkHeader) {
+                filterChain.doFilter(request, response);
+                return;
+            }
             return;
 
         }
 
         filterChain.doFilter(request, response);
 
+    }
+
+    private boolean checkAuthHeader(HttpServletRequest request) {
+
+        boolean result = false;
+
+        String authHeader = request.getHeader("Authorization");
+        if (StringUtils.hasText(authHeader)) {
+            log.info("Authorization exist : " + authHeader);
+            if (authHeader.equals("12345678")) {
+                result = true;
+            }
+        }
+        return result;
     }
 
 }
