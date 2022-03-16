27a8e67b35c1c14b6832f9e7e54775ec3731d002
kopro-ov <koprodev@gmail.com>
2022-03-16 22:48:37 +0900

    자동 로그인 설정

--- springboot/club/src/main/java/com/gugucoding/club/config/SecurityConfig.java
+++ springboot/club/src/main/java/com/gugucoding/club/config/SecurityConfig.java
@@ -1,11 +1,11 @@
 package com.gugucoding.club.config;
 
 import com.gugucoding.club.security.handler.ClubLoginSuccessHandler;
+import com.gugucoding.club.security.service.ClubUserDetailsService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private ClubUserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -30,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.logout();
 
         http.oauth2Login().successHandler(successHandler());
+        http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService);
 
     }
 
