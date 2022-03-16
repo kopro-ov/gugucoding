41d4288fb6a049901b960d44eb0b6dcfefef54b3
kopro-ov <koprodev@gmail.com>
2022-03-16 22:25:56 +0900

    소셜 회원 데이터베이스에 저장
    - OAuth2User를 이용하여 이메일 주소 획득
    - 이메일이 있으면 조회 없으면 신규 생성
    - 신규 생성시 임의로 패스워드를 지정해서 데이터베이스에 저장하는 경우 나중에 문제가 될 수 있지만, 우선 데이터베이스에 저장하는 코드 작성

--- springboot/club/src/main/java/com/gugucoding/club/security/service/ClubOAuthUserDetailsService.java
+++ springboot/club/src/main/java/com/gugucoding/club/security/service/ClubOAuthUserDetailsService.java
@@ -1,15 +1,27 @@
 package com.gugucoding.club.security.service;
 
+import com.gugucoding.club.entity.ClubMember;
+import com.gugucoding.club.entity.ClubMemberRole;
+import com.gugucoding.club.repository.ClubMemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class ClubOAuthUserDetailsService extends DefaultOAuth2UserService {
+
+    private final ClubMemberRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -29,7 +41,41 @@ public class ClubOAuthUserDetailsService extends DefaultOAuth2UserService {
             log.info(k + " : " +v);
         });
 
+        String email = null;
+
+        if (clientName.equals("Google")) {
+            email = oAuth2User.getAttribute("email");
+        }
+
+        log.info("EMAIL : "+email);
+
+        ClubMember member = saveSocialMember(email);
+
+
         return oAuth2User;
+
+    }
+
+    private ClubMember saveSocialMember(String email) {
+
+        //기존에 동일한 이메일로 가입한 회원이 있는 경우에는 그대로 조회만
+        Optional<ClubMember> result = repository.findByEmail(email, true);
+
+        if (result.isPresent()) {
+            return result.get();
+        }
+
+        //없다면 회원 추가 패스워드는 1111 이름은 그냥 이메일 주소로.
+        ClubMember clubMember = ClubMember.builder().email(email)
+                .name(email)
+                .password(passwordEncoder.encode("1111"))
+                .fromSocial(true)
+                .build();
+        clubMember.addMemberRole(ClubMemberRole.USER);
+
+        repository.save(clubMember);
+
+        return clubMember;
 
     }
 }
