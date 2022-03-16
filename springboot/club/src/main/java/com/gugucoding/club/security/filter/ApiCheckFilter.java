47461321134a55982fc37bc1231a8d725e822a3b
kopro-ov <koprodev@gmail.com>
2022-03-17 01:58:09 +0900

    checkHeader()가 false를 반환하면 JSON 데이터와 403 에러 메시지를 만들어서 전송

--- springboot/club/src/main/java/com/gugucoding/club/security/filter/ApiCheckFilter.java
+++ springboot/club/src/main/java/com/gugucoding/club/security/filter/ApiCheckFilter.java
@@ -1,6 +1,8 @@
 package com.gugucoding.club.security.filter;
 
 import lombok.extern.log4j.Log4j2;
+import net.minidev.json.JSONObject;
+import netscape.javascript.JSObject;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -10,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Log4j2
 public class ApiCheckFilter extends OncePerRequestFilter {
@@ -34,10 +37,20 @@ public class ApiCheckFilter extends OncePerRequestFilter {
             boolean checkHeader = checkAuthHeader(request);
             if (checkHeader) {
                 filterChain.doFilter(request, response);
-                return;
+            } else {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                //json 리턴 및 한글깨짐
+                response.setContentType("application/json;chart=utf-8");
+                JSONObject jsonObject = new JSONObject();
+                String message = "FAIL CHECK API TOKEN";
+                jsonObject.put("code", "403");
+                jsonObject.put("message", message);
+
+                PrintWriter out = response.getWriter();
+                out.println(jsonObject);
+
             }
             return;
-
         }
 
         filterChain.doFilter(request, response);
